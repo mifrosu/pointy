6dcb4e771a6a8f251a2c67c9b9168d5b6ad7f433
Michael O'Sullivan <mifrosu@gmail.com>
2013-05-13 19:13:32 +0100

    Initial main program Reads in model files and the pinpoint introduction.pin file from the command line.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
+#include "slide_data.h"
+#include "slide_list_model.h"
+#include <qdebug.h>
 #include <iostream>
 
-int main()
+
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello, this is Eddie your ship-board computer!"
+        if (argc > 1) {
+            std::cout << "argv: " << argv[1] <<std::endl;
+            QString fileName = argv[1];
+            pointy::SlideListModel showModel;
+            showModel.readSlideFile(fileName);
+            QStringList slideData = showModel.getRawSlideData();
+            QStringList::const_iterator iter = slideData.begin();
+            QStringList::const_iterator endIter = slideData.end();
+            while (iter != endIter) {
+                qDebug() <<  iter->toLatin1();
+                ++iter;
+            }
+        }
+    std::cout << "-----Completed-----"
                  << std::endl;
 
     return 0;
