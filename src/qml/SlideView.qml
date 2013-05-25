mifrosu/pointy

commit b20ec301966b046f5e07cedcd3f9edeef759adb8

adds additional checks to slideSettingAssign function

src/slide_data.cpp
@@ -26,7 +26,9 @@ void SlideData::slideSettingAssign(const QString &lhs_in,
     QString rhs(rhs_in.trimmed());
 
     if (lhs == "stage-color") {
-        this->stageColor = rhs;
+        if (QColor::isValidColor(rhs)) {
+            this->textColor = rhs;
+        }
     }
     else if (lhs == "font") {
         setFont(rhs);
@@ -47,7 +49,9 @@ void SlideData::slideSettingAssign(const QString &lhs_in,
         this->textAlign = rhs;
     }
     else if (lhs == "shading-color") {
-        this->shadingColor = rhs;
+        if (QColor::isValidColor(rhs)) {
+            this->textColor = rhs;
+        }
     }
     else if (lhs == "shading-opacity") {
         bool ok;
@@ -56,7 +60,10 @@ void SlideData::slideSettingAssign(const QString &lhs_in,
             return;
         }
         else {
-        this->shadingOpacity = temp;
+            if (temp >= 0.0 && temp <= 1.0) {
+                this->shadingOpacity = temp;
+            }
+
         }
     }
     else if (lhs == "duration") {

src/qml/PointySlide.qml
@@ -47,7 +47,10 @@ Rectangle {
                     return "blank.png";
                 }
             }
-            fillMode: Image.PreserveAspectFit;
+            fillMode: {
+                Image.PreserveAspectFit;
+            }
+
         }
     }
 
@@ -131,16 +134,16 @@ Rectangle {
     Loader {
         id: loadedComponent
         sourceComponent: {
-            if (slideMedia.match(/.jpeg|.jpg/i)) {
-                return slideImage;
-            }
-            else if (slideMedia.match(
+            if (slideMedia.match(
               /.avi|.flv|.mkv|.mov|.mp4|.mpeg|.ogv|.webm/i)) {
                 slideElement.isMediaSlide = true;
                 return videoComponent;
             }
             else if (slideMedia.match(/.gif/i)) {
                 return animatedComponent;
+            }
+            else {
+                return slideImage;
             }
         }
 

commit 9416fffdbf0d2c21f681fba25997752df30d6a99

no longer includes settings slide in presentation. corrects validation error introduced in commit b20ec301966b046f5e07cedcd3f9edeef759adb8 updates unit tests

src/qml/SlideView.qml
@@ -52,14 +52,7 @@ Rectangle {
 
         property int slideCount: count;
 
-        currentIndex: {
-            if (slideCount > 0){
-                return 1;
-            }
-            else {
-                return 0;
-            }
-        }
+        currentIndex: 0;
 
         Timer {
             id: waitTime;
@@ -91,7 +84,7 @@ Rectangle {
                 target: dataView;
                 // target: fadeRectangle;
                 properties: "opacity";
-                from: 1.0; to: 0.3; duration: 200;
+                from: 1.0; to: 0.3; duration: 500;
             }
             ScriptAction {
                 script: {
@@ -101,7 +94,7 @@ Rectangle {
             NumberAnimation {
                 target: dataView;
                 properties: "opacity";
-                from: 0.3; to: 1.0; duration: 200;
+                from: 0.3; to: 1.0; duration: 500;
             }
         }
 
@@ -163,7 +156,7 @@ Rectangle {
                 //currentIndex += 1;
             }
 
-            else if (event.key === Qt.Key_Backspace && currentIndex > 1) {
+            else if (event.key === Qt.Key_Backspace && currentIndex > 0) {
                 dataView.moveForward = false;
                 loadTransition(dataView.currentItem.pointyTransition);
                 //currentIndex -=1;

src/slide_data.cpp
@@ -27,7 +27,7 @@ void SlideData::slideSettingAssign(const QString &lhs_in,
 
     if (lhs == "stage-color") {
         if (QColor::isValidColor(rhs)) {
-            this->textColor = rhs;
+            this->stageColor = rhs;
         }
     }
     else if (lhs == "font") {
@@ -50,7 +50,7 @@ void SlideData::slideSettingAssign(const QString &lhs_in,
     }
     else if (lhs == "shading-color") {
         if (QColor::isValidColor(rhs)) {
-            this->textColor = rhs;
+            this->shadingColor = rhs;
         }
     }
     else if (lhs == "shading-opacity") {

src/slide_list_model.cpp
@@ -308,6 +308,9 @@ void SlideListModel::readSlideFile(const QString fileName)
 
                 // insert("slideText",*currentSlideText);
     }
+    if (!slideList.isEmpty()) {
+        slideList.pop_front();
+    }
 
 }
 

tests/pointy_test_file_read.cpp
@@ -15,29 +15,30 @@ void TestFileRead::readSimpleFile()
 //                           " comments with a hash\n"
 //                           " global settings before first slide");
     QStringList data = testModel->getRawSlideData();
-    QStringList expectedData = (QStringList() << "stageColor: black" <<
-                                "font: sans" <<
-                                "fontSize: 50" <<
-                                "fontSizeUnit: px" <<
-                                "notesFont: Sans" <<
-                                "notesFontSize: black" <<
-                                "textColor: white" <<
-                                "textAlign: left" <<
-                                "shadingColor: black" <<
-                                "shadingOpacity: 0.66" <<
-                                "duration: 5.5" <<
-                                "command: " <<
-                                "transition: fade" <<
-                                "cameraFrameRate: 0" <<
-                                "backgroundScale: fill" <<
-                                "position: center" <<
-                                "useMarkup: 1" <<
-                                "slideText: " <<
-                                "maxLineLength: 0" <<
-                                "slideMedia: " <<
-                                "backgroundColor: white" <<
-                                "notesText: " <<
-                                "slideNumber: 0" <<
+    QStringList expectedData = (QStringList() <<
+//                                "stageColor: black" <<
+//                                "font: sans" <<
+//                                "fontSize: 50" <<
+//                                "fontSizeUnit: px" <<
+//                                "notesFont: Sans" <<
+//                                "notesFontSize: black" <<
+//                                "textColor: white" <<
+//                                "textAlign: left" <<
+//                                "shadingColor: black" <<
+//                                "shadingOpacity: 0.66" <<
+//                                "duration: 5.5" <<
+//                                "command: " <<
+//                                "transition: fade" <<
+//                                "cameraFrameRate: 0" <<
+//                                "backgroundScale: fill" <<
+//                                "position: center" <<
+//                                "useMarkup: 1" <<
+//                                "slideText: " <<
+//                                "maxLineLength: 0" <<
+//                                "slideMedia: " <<
+//                                "backgroundColor: white" <<
+//                                "notesText: " <<
+//                                "slideNumber: 0" <<
                                 "stageColor: black" <<
                                 "font: sans" <<
                                 "fontSize: 50" <<

tests/pointy_test_slide_setting.cpp
@@ -18,8 +18,8 @@ void TestSlideSetting::setFontTest()
 
 void TestSlideSetting::assignSlideSettings()
 {
-    testSlide->slideSettingAssign("stage-color "," black");
-    QCOMPARE(testSlide->stageColor, QString("black"));
+    testSlide->slideSettingAssign("stage-color "," red");
+    QCOMPARE(testSlide->stageColor, QString("red"));
     testSlide->slideSettingAssign("font", "Sans 50 px");
     QCOMPARE(testSlide->font, QString("sans"));
     QCOMPARE(testSlide->fontSize, qreal(50));
@@ -32,8 +32,8 @@ void TestSlideSetting::assignSlideSettings()
     QCOMPARE(testSlide->textColor, QString("crimson"));
     testSlide->slideSettingAssign("text-align","left");
     QCOMPARE(testSlide->textAlign, QString("left"));
-    testSlide->slideSettingAssign("shading-color", "blue");
-    QCOMPARE(testSlide->shadingColor, QString("blue"));
+    testSlide->slideSettingAssign("shading-color", "red");
+    QCOMPARE(testSlide->shadingColor, QString("red"));
 
     testSlide->slideSettingAssign("shading-opacity","0.5");
     QCOMPARE(testSlide->shadingOpacity, qreal(0.5));